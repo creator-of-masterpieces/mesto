683578404a8515570f9456c2b8be056faa652faf
Dmitrii Timonov <timonov.dim13@gmail.com>
2020-11-15 13:52:03 +0300

    feat: Сделал загрузку карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,56 @@
+// Переменные с элементами попапа
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__button-close');
 const editButton = document.querySelector('.profile__button_type_edit');
-
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
-
 const form = document.querySelector('.popup__form');
 const formInputName = document.querySelector('.popup__input_type_name');
 const formInputJob = document.querySelector('.popup__input_type_job');
 
+
+// Переменные с элементами секции профиль
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+// Переменные с элементами секции template-elements
+const initialCards = [
+	{
+			name: 'Архыз',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+	},
+	{
+			name: 'Челябинская область',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	},
+	{
+			name: 'Иваново',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+	},
+	{
+			name: 'Камчатка',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+	},
+	{
+			name: 'Холмогорский район',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+	},
+	{
+			name: 'Байкал',
+			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+	}
+];
+
+const cardsComtainer =  document.querySelector('.elements');
+
+
+
+//Открывает попап для редактирование профиля
 function showPopup() {
 	popup.classList.add('popup_opened');
 	formInputName.value = profileTitle.textContent
 	formInputJob.value = profileSubtitle.textContent
 }
 
+//Закрывает попап для редактирование профиля
 function closePopup() {
 	popup.classList.remove('popup_opened')
 	formInputName.value = ''
@@ -27,6 +63,7 @@ function popupClickHandler(event) {
 	}
 }
 
+// Сохраняет данные профиля
 function submitForm(event) {
 	event.preventDefault()
 	profileTitle.textContent = formInputName.value;
@@ -35,8 +72,22 @@ function submitForm(event) {
 }
 
 
+// Назначает события на клики по кнопкам
 editButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', closePopup);
 popup.addEventListener('mousedown', popupClickHandler);
 form.addEventListener('submit', submitForm);
 
+//Загружает карточки
+function addCards (card) {
+	const templateCards = document.querySelector('.template-elements').content.cloneNode(true);
+	const cardName = card.name;
+	const imageLink = card.link;
+	templateCards.querySelector('.elements__title').textContent = cardName;
+	templateCards.querySelector('.elements__image').setAttribute('src', imageLink);
+
+	cardsComtainer.append(templateCards);
+}
+
+// Заполняет карточки информацией из массива initialCards
+initialCards.forEach(addCards);
